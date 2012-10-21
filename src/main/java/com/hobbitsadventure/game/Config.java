0186bfc83f5ff823363a1423368ab02e2c2157c1
Jude Wheeler <judeswheeler@gmail.com>
2012-10-21 09:19:36 -0700

    Test commit

--- src/main/java/com/hobbitsadventure/game/Config.java
+++ src/main/java/com/hobbitsadventure/game/Config.java
@@ -1,8 +1,5 @@
 package com.hobbitsadventure.game;
 
-/**
- * @author Willie Wheeler (willie.wheeler@gmail.com)
- */
 public class Config {
 	
 	// Set GOD_MODE = true to walk through walls, rocks, etc.
